f6ccded6bee208e749cacf633e5b7c5f521aeed1
PAG-Erik <erikdavinci@gmail.com>
2023-05-23 21:52:08 -0300

    feature: Add put() method to Cerebro redis

--- src/Utils/Cache/Cerebro.php
+++ src/Utils/Cache/Cerebro.php
@@ -7,7 +7,17 @@ class Cerebro
     private static bool $retrieved_from_cache;
     private const DEFAULT_TTL_IN_SECONDS = 60;
 
-    public static function get(string $key, callable $callback, int $ttl = self::DEFAULT_TTL_IN_SECONDS)
+    public static function put(string $key, mixed $value, int $ttl = self::DEFAULT_TTL_IN_SECONDS): bool
+    {
+        $value = self::prepareValue($value);
+        $redis = new \Redis();
+        $redis->connect('redis', 6379, 3.5);
+        $success = $redis->setex($key, $ttl, $value);
+
+        return (bool) $success;
+    }
+
+    public static function get(string $key, callable $callback = null, int $ttl = self::DEFAULT_TTL_IN_SECONDS)
     {
         $redis = new \Redis();
         $redis->connect( 'redis', 6379, 3.5);
@@ -16,6 +26,10 @@ class Cerebro
 
         if (empty($value)) {
             self::$retrieved_from_cache = false;
+            if (! $callback) {
+                return null;
+            }
+
             $value = $callback();
             $redis->setex($key, $ttl, self::prepareValue($value));
         }
@@ -50,6 +64,10 @@ class Cerebro
     private static function prepareRetrievedValue(mixed $value)
     {
         if (is_string($value)) {
+            $decoded_value = json_decode($value);
+            if (json_last_error() == JSON_ERROR_NONE) {
+                return $decoded_value;
+            }
             return @unserialize($value) ?: $value;
         } else {
             return $value;
